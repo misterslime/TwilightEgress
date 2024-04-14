e790d7e50a05007fbc9ced44ea02edffb293fabf
flats <hiimhibiscus@gmail.com>
2024-04-14 19:23:09 -0400

    experimental planetoid rework i think

---
+++ Common/Systems/PlanetoidSystem/Planetoid.cs
@@ -0,0 +1,25 @@
+﻿namespace Cascade.Common.Systems.PlanetoidSystem {
+    public abstract class Planetoid : Entity {
+        public int ID;
+        public virtual float Gravity { get; set; } = 0.0f;
+        public virtual bool doesCollide { get; set; }
+
+        public virtual void Update() { }
+        public virtual void Collision(Player player) { }
+
+        public void AI() {
+            if(doesCollide) {
+                for(int i = 0; i < Main.maxPlayers; i++) {
+                    Player player = Main.player[i];
+                    if(Colliding(player))
+                        Collision(player);
+                }
+            }
+            Update();
+        }
+
+        public virtual bool Colliding(Player player) 
+            => player.Hitbox.Intersects(Hitbox);
+
+    }
+}
---
+++ Common/Systems/PlanetoidSystem/PlanetoidSystem.cs
@@ -0,0 +1,22 @@
+﻿namespace Cascade.Common.Systems.PlanetoidSystem {
+    public class PlanetoidSystem : ModSystem {
+
+        public Dictionary<Type, Planetoid> planetoidsByType = new();
+        public PlanetoidSystem() { }
+
+        public override void Load() {
+            On_Player.DryCollision += (orig, self, fallThrough, ignorePlats) => {
+                
+            };
+            base.Load();
+        }
+    }
+
+    public class PlanetoidPlayer : ModPlayer {
+
+        public Planetoid? planetoid = null;
+        public float angle = 0;
+
+        public PlanetoidPlayer() { }
+    }
+}
