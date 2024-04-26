c4e1385490cb3bf5dc0382d7ba1fc9b07c6ca679
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-04-26 11:33:30 -0600

    Final palette !!!!

--- Common/Utilities/ColorUtilities.cs
+++ Common/Utilities/ColorUtilities.cs
@@ -2,15 +2,26 @@
 {
     public static partial class CascadeUtilities
     {
-        public static readonly Vector4[] CosmostonePalette = new Vector4[]
+        public static readonly Vector4[] CosmostonePalette =
         {
-            new Color(99, 217, 254).ToVector4(),
-            new Color(1, 158, 253).ToVector4(),
-            new Color(2, 80, 253).ToVector4(),
-            new Color(50, 39, 199).ToVector4(),
-            new Color(90, 66, 218).ToVector4(),
-            new Color(134, 50, 226).ToVector4(),
-            new Color(172, 50, 227).ToVector4(),
+            new Color(96, 188, 246).ToVector4(),
+            new Color(81, 158, 245).ToVector4(),
+            new Color(76, 131, 242).ToVector4(),
+            new Color(3, 96, 243).ToVector4(),
+            new Color(48, 65, 197).ToVector4(),
+            new Color(104, 94, 228).ToVector4(),
+            new Color(157, 113, 239).ToVector4(),
         };
+
+        public static Color HexToRBG(string hexColor)
+        {
+            int color = Convert.ToInt32(hexColor, 16);
+
+            int r = (color & 0xff0000) >> 16;
+            int g = (color & 0xff00) >> 8;
+            int b = (color & 0xff);
+
+            return new Color(r, g, b);
+        }
     }
 }
