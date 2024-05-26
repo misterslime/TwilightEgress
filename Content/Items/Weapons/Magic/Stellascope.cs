856ae0e67caadd45fff9abcab32f4b4d29ab83a7
JupiterO5 <alyssa.eclipsingo5@gmail.com>
2024-05-25 21:02:28 -0600

    First bit of stellascope

---
+++ Content/Items/Weapons/Magic/Stellascope.cs
@@ -0,0 +1,31 @@
+﻿using Cascade.Content.Projectiles.Magic;
+
+namespace Cascade.Content.Items.Weapons.Magic
+{
+    public class Stellascope : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 25;
+            Item.height = 8;
+            Item.DamageType = DamageClass.Magic;
+            Item.damage = 24;
+            Item.useTime = Item.useAnimation = 20;
+            Item.knockBack = 0;
+            Item.mana = 12;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shoot = ModContent.ProjectileType<StellascopeHoldout>();
+            Item.channel = true;
+            Item.value = 12504;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.rare = ItemRarityID.Green;
+        }
+        public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+        {
+            Projectile.NewProjectile(source, position, velocity, ModContent.ProjectileType<StellascopeHoldout>(), damage, knockback, player.whoAmI);
+            return false;
+        }
+        public override bool CanUseItem(Player player) => player.ownedProjectileCounts[Item.shoot] <= 0;
+    }
+}
---
+++ Content/Projectiles/Magic/StellascopeHoldout.cs
@@ -0,0 +1,50 @@
+﻿namespace Cascade.Content.Projectiles.Magic
+{
+    public class StellascopeHoldout : ModProjectile
+    {
+        private static Asset<Texture2D> HoldoutTexture;
+        private Player Owner => Main.player[Projectile.owner];
+
+        public override void Load() => HoldoutTexture = ModContent.Request<Texture2D>("Cascade/Content/Projectiles/Magic/StellascopeHoldout");
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = DamageClass.Magic;
+            Projectile.friendly = true;
+            Projectile.ignoreWater = true;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+        }
+        public override void AI()
+        {
+            Projectile.velocity = Vector2.Normalize(Main.MouseWorld - Owner.MountedCenter);
+            Projectile.rotation = Projectile.velocity.ToRotation() + (Projectile.direction == 1 ? 0f : Pi);
+            Projectile.Center = Owner.Center;
+            UpdatePlayerVariables();
+
+            if (CalamityUtils.CantUseHoldout(Owner))
+                Projectile.Kill();
+        }
+        private void UpdatePlayerVariables()
+        {
+            Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, 0 - Pi / 1.5f * Projectile.direction);
+            Owner.itemTime = 2;
+            Owner.itemAnimation = 2;
+            Owner.itemRotation = (Projectile.velocity * Projectile.direction).ToRotation();
+            Owner.ChangeDir(Projectile.direction);
+            Owner.heldProj = Projectile.whoAmI;
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Vector2 drawPosition = Projectile.Center - Main.screenPosition;
+            drawPosition = new Vector2(drawPosition.X + 10 * Projectile.direction, drawPosition.Y - 8);
+            Rectangle source = new Rectangle(0, 0, HoldoutTexture.Width(), HoldoutTexture.Height());
+            Vector2 rotationOrigin = HoldoutTexture.Size() * 0.5f;
+            rotationOrigin.X -= 20 * Projectile.direction;
+            SpriteEffects spriteFlip = Projectile.direction == 1 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            Main.EntitySpriteDraw(HoldoutTexture.Value, drawPosition, source, lightColor, Projectile.rotation, rotationOrigin, 1f, spriteFlip);
+            return false;
+        }
+        public override bool ShouldUpdatePosition() => false;
+        public override bool? CanDamage() => false;
+    }
+}
