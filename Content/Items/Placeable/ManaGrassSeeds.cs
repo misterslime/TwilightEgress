3bcee946850e1a5c3bf69c516db5bcd49ef765c2
Nuclear Chaos <30942693+misterslime@users.noreply.github.com>
2024-08-05 02:54:55 -0600

    no automatic pinging in ci + gutted mana grass

--- Content/Items/Placeable/ManaGrassSeeds.cs
+++ Content/Items/Placeable/ManaGrassSeeds.cs
@@ -1,11 +1,4 @@
-﻿using Cascade.Content.Tiles.ManaPollution;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cascade.Content.Items.Placeable
+﻿namespace Cascade.Content.Items.Placeable
 {
     public class ManaGrassSeeds : ModItem, ILocalizedModType
     {
@@ -27,23 +20,6 @@ namespace Cascade.Content.Items.Placeable
             Item.useAnimation = 15;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.maxStack = 9999;
-        }
-
-        public override bool? UseItem(Player player)
-        {
-            Tile tile = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY);
-            Tile tileAbove = Framing.GetTileSafely(Player.tileTargetX, Player.tileTargetY - 1);
-
-            if (tile.HasTile && !tileAbove.HasTile && tileAbove.LiquidAmount == 0 && /*tile.TileType == ModContent.TileType<Tiles.Crags.ScorchedRemains>() &&*/ player.IsInTileInteractionRange(Player.tileTargetX, Player.tileTargetY, TileReachCheckSettings.Simple))
-            {
-                Main.tile[Player.tileTargetX, Player.tileTargetY].TileType = (ushort)ModContent.TileType<ManaGrass>();
-
-                SoundEngine.PlaySound(SoundID.Dig, player.Center);
-
-                return true;
-            }
-
-            return false;
         }
     }
 }
